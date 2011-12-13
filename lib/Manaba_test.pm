c8e8e6500ab6783898cee1a92edc40e4eba56bcd
Hanyoung Cho <rumidier@gmail.com>
2011-12-13 20:45:30 +0900

    yaml에서 정보를 읽어온후 확인

--- lib/Manaba_test.pm
+++ lib/Manaba_test.pm
@@ -3,8 +3,22 @@ use Dancer ':syntax';
 
 our $VERSION = '0.1';
 
+use 5.010;
+use YAML::Tiny;
+use Data::Dumper;
+
+my $CONFIG;
+
 get '/' => sub {
     template 'index';
 };
+
+sub load_manaba {
+    my $yaml = YAML::Tiny->read( config->{manaba} );
+    $CONFIG = $yaml->[0];
+}
+
+load_manaba();
+debug Dumper \$CONFIG;
 
 true;
