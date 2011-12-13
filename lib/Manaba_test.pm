dca4977cbf81ce2c243c6577ff4a879d7746ae65
Hanyoung Cho <rumidier@gmail.com>
2011-12-13 22:03:08 +0900

    SCRAPER data

--- lib/Manaba_test.pm
+++ lib/Manaba_test.pm
@@ -8,10 +8,55 @@ use YAML::Tiny;
 use Data::Dumper;
 
 my $CONFIG;
+my $SCRAPERS = {
+    daum => scraper {
+        process(
+            'div.episode_list > div.inner_wrap > div.scroll_wrap > ul > li',
+            'items[]',
+            scraper {
+                process 'a.img', link  => '@href';
+                process 'a.img', title => '@title';
+            }
+        );
+    },
+    naver => scraper {
+        process(
+                'table.viewList tr td.tilte',
+                'imtes[]',
+            scraper {
+                process 'a', link => '@href';
+            }
+       );
+    },
+    nate => scraper {
+        process(
+                'div.wrap_carousel div.thumbPage div.thumbSet dd',
+                'imtes[]',
+            scraper {
+                process 'a',   link  => '@href';
+                process 'img', title => '@alt';
+            }
+       );
+    },
+};
 
 get '/' => sub {
     template 'index';
 };
+
+sub update {
+    my $id = shift;
+    return unless $id;
+
+    my $webtoon = $CONFIG->{webtoon};
+    return unless $webtoon;
+
+    my $site_name = $webtoon->{$id}{site};
+    return unless $site_name;
+
+    my $scraper = $SCRAPERS->{ $site_name };
+    return unless $scraper;
+}
 
 sub load_manaba {
     my $yaml = YAML::Tiny->read( config->{manaba} );
@@ -19,6 +64,6 @@ sub load_manaba {
 }
 
 load_manaba();
-debug Dumper \$CONFIG;
+update('noblesse');
 
 true;
